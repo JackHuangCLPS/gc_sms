e66d1047a0c09b26a3b826fa0dde3493506e3b58
JackHuangCLPS <JackHuang_CLPS@126.com>
2018-11-05 14:59:30 +0800

    再次上传

---
+++ src/main/java/com/gc/controller/AccountController.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name:clps.sms
+ * File Name:AccountController.java
+ * Package Name:com.clps.dev.sms.controller
+ * Date:2018年10月31日下午2:11:19
+ * Copyright (c) 2018,http://www.clpsglobal.com/zh/home-cn/ All Rights Reserved.
+ *
+ */
+package com.gc.controller;
+
+/**
+ * ClassName: AccountController <br/>
+ * Description: 账户控制层
+ *
+ * @author Jack.Huang
+ * @version V1.0
+ * @since JDK 1.8
+   @date:2018年10月31日 下午2:11:19
+ */
+public class AccountController {
+
+}
