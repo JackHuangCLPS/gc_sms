da89313dd119b70bb8eccb69243670a78125981c
JackHuangCLPS <JackHuang_CLPS@126.com>
2018-11-05 13:02:17 +0800

    首次提交

---
+++ src/main/java/com/gc/util/factory/ToolFactory.java
@@ -0,0 +1,28 @@
+/**
+ * Project Name:clps.sms
+ * File Name:ToolFactory.java
+ * Package Name:com.clps.dev.sms.util.factory
+ * Date:2018年10月31日下午2:19:18
+ * Copyright (c) 2018,http://www.clpsglobal.com/zh/home-cn/ All Rights Reserved.
+ *
+ */
+package com.gc.util.factory;
+
+import com.gc.util.date.DateTool;
+
+/**
+ * ClassName: ToolFactory <br/>
+ * Description: 工具工厂
+ *
+ * @author Jack.Huang
+ * @version V1.0
+ * @since JDK 1.8
+   @date:2018年10月31日 下午2:19:18
+ */
+public class ToolFactory {
+	
+	public static DateTool getInstanceOfDateTool() {
+		return new DateTool();
+	}
+
+}
